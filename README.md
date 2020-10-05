d837e5a29f23f32c88d10e1be3df8732e3e378a6
Ria Sanghvi <67433286+R29sanghvi@users.noreply.github.com>
2020-10-05 09:49:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SeigeTowerC30
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sling.js
@@ -0,0 +1,30 @@
+class Sling {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.sling);
+    }
+    display(color) {
+        if (this.sling.bodyA) {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke(color);
+            strokeWeight(7);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
+        }
+    }
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+}
